ac0e57cc6110dcb35af837304e9c6fb4f49099fe
xuyingzhou <xuyingzhou@tuzhanai.com>
2026-03-19 15:17:18 +0800

    Turn 6698a3ed at 2026-03-19T07:17:18.428Z

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Hello World test file"""
 
-def hello():
-    print("Hello, World!")
+def hello(name="World"):
+    print(f"Hello, {name}!")
 
 if __name__ == "__main__":
     hello()
+    hello("Python")
